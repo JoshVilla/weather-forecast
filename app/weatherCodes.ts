44dc39e4aa5eaec33717f3d07516e49cc13dda7f
JoshVilla <villaearljoshua@gmail.com>
2025-04-01 11:16:51 +0800

    fix merge conflict

--- app/weatherCodes.ts
+++ app/weatherCodes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const weatherClearCode = [1000];
 export const weatherPartlyCloudyCode = [1003];
 export const weatherCloudyCode = [1006, 1009, 1030];
@@ -8,14 +7,3 @@ export const weatherDrizzleCode = [1063, 1150, 1153, 1180, 1135, 1147, 1240];
 export const weatherSnowCode = [1117, 1210, 1216];
 export const weatherLightningCode = [1087];
 export const weatherHeavyRainCode = [1243, 1246];
-=======
-export const weatherClearCode = [1000];
-export const weatherPartlyCloudyCode = [1003];
-export const weatherCloudyCode = [1006, 1009, 1030];
-export const weatherRainCode = [1183, 1186, 1192, 1195];
-export const weatherThunderstormCode = [1273, 1276];
-export const weatherDrizzleCode = [1063, 1150, 1153, 1180, 1135, 1147, 1240];
-export const weatherSnowCode = [1117, 1210, 1216];
-export const weatherLightningCode = [1087];
-export const weatherHeavyRainCode = [1243, 1246];
->>>>>>> 0fa559792e86efdb4f53ff5ec757a2b79958feb4
